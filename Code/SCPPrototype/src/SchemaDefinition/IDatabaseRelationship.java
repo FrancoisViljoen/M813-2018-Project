863dcfafe9da43dc76a70a576c782cdc8ca322a5
Francois Viljoen <francois.viljoen@llamasoft.com>
2018-04-11 15:13:44 +0100

    Initial Schema Interface Creation

---
+++ Code/SCPPrototype/src/SchemaDefinition/IDatabaseRelationship.java
@@ -0,0 +1,17 @@
+/*
+ * License terms to be defined.
+ * Currently - Copyright Reserved 2018
+ * Francois Viljoen
+ */
+package SchemaDefinition;
+
+/**
+ * Representation of a Relationship between two database tables.
+ * Typically a Foreign Key, but could also be a manually defined
+ * relationship.
+ * 
+ * @author Francois Viljoen
+ */
+public interface IDatabaseRelationship {
+    
+}
